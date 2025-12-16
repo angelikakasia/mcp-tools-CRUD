66bdc8fe849f47244f94670868cb1e04e02a75f0
suzanaai <angelska327@gmail.com>
2025-12-16 10:46:53 -0600

    libraries

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // server.js
 require('dotenv').config();
 const express = require('express');
-const mongoose = require('mongoose');   // <-- YOU WANT THIS HERE
+const mongoose = require('mongoose');  
 const methodOverride = require('method-override');
 const session = require('express-session');
 const MongoStore = require('connect-mongo');
