49b7b23e21001f33870117557b8a0ee24ebec546
suzanaai <angelska327@gmail.com>
2025-12-18 13:05:16 -0600

    index.ejs prettying thins up

--- middleware/isLoggedIn.js
+++ middleware/isLoggedIn.js
@@ -1,8 +1,8 @@
 module.exports = (req, res, next) => {
-    if (req.session.user){
-        //user is looged in - allow access
-        return next();
-    }
-    //user is NOT logged in redirect to login
-    res.redirect('/login');
+  if (req.session.user) {
+    // user is logged in — allow access
+    return next();
+  }
+  // user is NOT logged in — redirect to login
+  res.redirect('/auth/login');
 };
